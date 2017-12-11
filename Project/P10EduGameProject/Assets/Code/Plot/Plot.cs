f7e0580f9053ac1ae6a8172184f553fda98f8b52
Bowen Harkema <newob01@hotmail.nl>
2017-12-11 13:38:48 +0100

    going to update project to new design

--- Project/P10EduGameProject/Assets/Code/Plot/Plot.cs
+++ Project/P10EduGameProject/Assets/Code/Plot/Plot.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class Plot
 {
+    public string _MoleculeType;
     public bool _Useable;
+
+    [Space]
+
     public int _Stage = 0;
     //public GameObject[] _UseStages;
     public GameObject[] _FlowerStages;
